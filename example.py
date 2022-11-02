55781bb0bbedbe7387a9e705ee61c7069ec3415c
mlevental <maksim.levental@gmail.com>
2022-11-02 13:26:24 -0500

    Refactor to work with multiple nodes

--- example.py
+++ example.py
@@ -1,3 +1,6 @@
+import json
+import sys
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,4 +12,17 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    config_json = json.load(open("config.json"))
+    id = int(sys.argv[1])
+
+    my_ip, my_port = None, None
+    peers = []
+    for i, address in enumerate(config_json["addresses"]):
+        ip, port = address["ip"], str(address["port"])
+        if i == id:
+            my_ip, my_port = ip, port
+        else:
+            peers.append((ip, port))
+
+    assert my_port
+    app.run(debug=True, host="localhost", port=my_port, threaded=True)
