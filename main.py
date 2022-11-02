5912d2b8914b7e6c0fd098d40b83487e7732cda8
mlevental <maksim.levental@gmail.com>
2022-11-02 14:20:47 -0500

    Hookup the timer to the Raft node and check that RPCs can be sent an received

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import json
 import sys
-import time
 
+import routes
+from raft import Node
 from routes import app
-from timer import ResettableTimer
-from logg import debug_print
 
 
 def parse_config_json(fp, idx):
@@ -23,22 +22,10 @@ def parse_config_json(fp, idx):
     return my_ip, my_port, peers
 
 
-start_time = time.time()
-
-
-def log_time_taken():
-    global start_time
-    debug_print("Time's Up! Took ", time.time() - start_time, "seconds")
-    start_time = time.time()
-    timer.reset()
-
-
 if __name__ == "__main__":
     config_json_fp = sys.argv[1]
     config_json_idx = int(sys.argv[2])
     _my_ip, my_port, peers = parse_config_json(config_json_fp, config_json_idx)
 
-    timer = ResettableTimer(log_time_taken, interval_lb=1000, interval_ub=2000)
-    timer.run()
-
+    routes.raft_node = Node(config_json_idx, peers)
     app.run(debug=True, host="localhost", port=my_port, threaded=True)
--- routes.py
+++ routes.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
+from raft import Node
 
 app = Flask(__name__)
 
+raft_node: Node = None
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route("/request-vote/<id>", methods=["POST"])
+def request_vote(id):
+    rpc_message_json = request.json
+    res = raft_node.rpc_handler(id, rpc_message_json)
+    return jsonify(res)
